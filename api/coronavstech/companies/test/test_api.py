bdcac173da0b357624c52ee0f094b0343261c8fa
JingCao <2410564208@qq.com>
2021-07-30 11:58:32 +0800

    pytest tests that assert exceptions

--- api/coronavstech/companies/test/test_api.py
+++ api/coronavstech/companies/test/test_api.py
@@ -93,3 +93,11 @@ class TestPostCompanies(BasicCompanyApiTestCase):
         self.assertEqual(1, 2)
 
 
+# test exception
+def raise_covid19_exception() -> None:
+    raise ValueError("CoronaVirus Exception")
+
+def test_raise_covid19_exception_should_pass() -> None:
+    with pytest.raises(ValueError) as e:
+        raise_covid19_exception()
+    assert "CoronaVirus Exception" == str(e.value)
