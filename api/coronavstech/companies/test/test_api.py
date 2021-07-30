d30183d1837266d90a883f8af5cf55c3ac3427c0
JingCao <2410564208@qq.com>
2021-07-30 12:14:04 +0800

    pytest tests that assert logs

--- api/coronavstech/companies/test/test_api.py
+++ api/coronavstech/companies/test/test_api.py
@@ -101,3 +101,28 @@ def test_raise_covid19_exception_should_pass() -> None:
     with pytest.raises(ValueError) as e:
         raise_covid19_exception()
     assert "CoronaVirus Exception" == str(e.value)
+
+
+# test logs
+# logging levels:       debug  info  warning  error  critical
+# corresponding level:   10     20     30      40      50
+import logging
+logger = logging.getLogger("CORONA_LOGS")
+
+def function_that_logs_something() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        # 只log warning level
+        logger.warning(f"I am logging {str(e)}")
+
+# 测试 warning level的log
+def test_logged_warning_level(caplog) -> None:
+    function_that_logs_something()
+    assert "I am logging CoronaVirus Exception" in caplog.text
+
+# 测试 info level的log
+def test_logged_info_level(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        logger.info("I am logging info level")
+        assert "I am logging info level" in caplog.text
