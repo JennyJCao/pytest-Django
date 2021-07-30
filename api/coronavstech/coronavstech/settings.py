d0de73dc15819799ad7d35738e8b740c0418cd12
JingCao <2410564208@qq.com>
2021-07-30 23:36:10 +0800

    adding an email sending service and test it with unittest

--- api/coronavstech/coronavstech/settings.py
+++ api/coronavstech/coronavstech/settings.py
@@ -125,3 +125,13 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# send emails setting
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "israeltechlayoffs@gmail.com"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_PASSWORD = "!ADS213FGBC!!"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
