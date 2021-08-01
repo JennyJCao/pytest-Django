8a2d9716fce636dd582f8d9d7966b357a52af22c
JingCao <2410564208@qq.com>
2021-08-01 17:17:35 +0800

    implementing from scratch pytests's parametrize feature

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -1,8 +1,15 @@
 from fibonacci.naive import fibonacci_naive
 import pytest
+from my_decorator import my_parametrized
 
 
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 def test_naive(n: int, expected: int) -> None:
+    res = fibonacci_naive(n=n)
+    assert res == expected
+
+# create our own parametrize and use it to test naive
+@my_parametrized(identifiers = "n,expected", values = [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_naive_with_my_decorator(n: int, expected: int) -> None:
     res = fibonacci_naive(n=n)
     assert res == expected
