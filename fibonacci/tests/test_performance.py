93db4d6f5e44d0bb030275c6d4c61881a7999f70
JingCao <2410564208@qq.com>
2021-08-01 18:44:14 +0800

    implementing a pytest performance validator

---
+++ fibonacci/tests/test_performance.py
@@ -0,0 +1,11 @@
+# from time import sleep
+
+import pytest
+from conftest import track_performance
+from fibonacci.dynamic import fibonacci_dynamic_v2
+
+@pytest.mark.performance
+@track_performance
+def test_performance():
+    # sleep(3)
+    fibonacci_dynamic_v2(1000)
