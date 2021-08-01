cf8a115a9e675963c497911c256550d864121544
JingCao <2410564208@qq.com>
2021-08-01 18:17:21 +0800

    using dynamic programming to solve fibonacci

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -1,6 +1,7 @@
 from typing import Callable
 
 import pytest
+from fibonacci.dynamic import fibonacci_dynamic, fibonacci_dynamic_v2
 from fibonacci.naive import fibonacci_naive
 from fibonacci.cached import fibonacci_cached
 from fibonacci.cached import fibonacci_lru_cached
@@ -8,7 +9,7 @@ from fixtures import time_tracker
 
 
 @pytest.mark.parametrize(
-    "fib_fun", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached]
+    "fib_fun", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached, fibonacci_dynamic, fibonacci_dynamic_v2]
 )
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 # Callable[[int], int]  receive an int and return an int
